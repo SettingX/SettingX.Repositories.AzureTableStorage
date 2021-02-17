22af141df0cd30b0309408f2963ed7283de1b80b
Nika Gamkrelidze <nikagamkrelidze233@gmail.com>
2021-02-17 17:44:39 +0400

    settings fetch added.

--- SettingX.Repositories.AzureTableStorage/Program.cs
+++ SettingX.Repositories.AzureTableStorage/Program.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Linq;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using SettingX.Repositories.AzureTableStorage.Settings;
 
 namespace SettingX.Repositories.AzureTableStorage
 {
@@ -8,12 +11,51 @@ namespace SettingX.Repositories.AzureTableStorage
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                int httpPort = 5000;
+                Host.CreateDefaultBuilder(args)
+                    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                    .ConfigureAppConfiguration(builder =>
+                    {
+                        var settingsUrl = Environment.GetEnvironmentVariable("SETTINGS_URL");
+                        builder.AddWebOrLocalSettingsConfiguration(settingsUrl);
+                    })
+                    .ConfigureWebHostDefaults(
+                        webBuilder =>
+                        {
+                            webBuilder.UseStartup<Startup>();
+                            webBuilder.ConfigureKestrel((ctx, opts) =>
+                            {
+                                if (ctx.HostingEnvironment.IsDevelopment())
+                                {
+                                    var urlsString = ctx.Configuration["ASPNETCORE_URLS"];
+                                    var urls = urlsString.Split(',', ';', StringSplitOptions.RemoveEmptyEntries);
+                                    var url = urls.FirstOrDefault(u => u.StartsWith("http://"));
+                                    if (url != null)
+                                    {
+                                        var parts = url.TrimEnd().Split(':');
+                                        if (int.TryParse(parts[parts.Length - 1], out var parsedPort))
+                                            httpPort = parsedPort;
+                                    }
+                                }
+                                else
+                                {
+                                    var httpPortSetting = ctx.Configuration["HttpPort"];
+                                    if (!string.IsNullOrWhiteSpace(httpPortSetting)
+                                        && int.TryParse(httpPortSetting, out var parsedHttpPort))
+                                        httpPort = parsedHttpPort;
+                                    webBuilder.UseUrls($"http://*:{httpPort}");
+                                }
+                            });
+                        })
+                    .Build()
+                    .Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
